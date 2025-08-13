3214e29710b89c2d06ed7eaa3a8ab11e3d29f5c7
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-08-13 02:50:00 +0100

    Update call_generative_prompt.py

--- artifact/gpt/workspace/call_generative_prompt.py
+++ artifact/gpt/workspace/call_generative_prompt.py
@@ -5,7 +5,7 @@ vocab_size = torch.load("/gpt/vocab_size.pt", weights_only=False)
 model = create_model(vocab_size)
 
 print("Loading trained model weight data from /gpt/trained_weights.pt ...")
-trained_weights = torch.load("/torch/trained_weights.pt")
+trained_weights = torch.load("/gpt/trained_weights.pt")
 model.load_state_dict(trained_weights)
 
 print("Loading stoi from /gpt/stoi.pt ...")
@@ -15,3 +15,4 @@ print("Loading itos from /gpt/itos.pt ...")
 itos = torch.load("/gpt/itos.pt", weights_only=False)
 
 generative_prompt(model, stoi, itos)
+
